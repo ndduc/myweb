ef32469e8511e3dbdb381f00096cbce32d2c834b
ndduc <ndduc1856@gmail.com>
2020-03-06 21:23:19 -0700

    update

---
+++ WebContent/script/project.js
@@ -0,0 +1,46 @@
+window.onload=()=>{ 
+// Initial state
+var scrollPos = 0;
+// adding scroll event
+var count = 1;
+window.addEventListener('scroll', function(){
+  // detects new state and compares it with the new one
+  if ((document.body.getBoundingClientRect()).top > scrollPos) {
+	console.log("UP");
+	//remove();
+  }
+		
+  else {
+	console.log("DOWN");
+	/*if(count == 10) {*/
+		add();
+	/*}*/
+	count ++;
+  }
+
+  console.log(count);
+		
+	scrollPos = (document.body.getBoundingClientRect()).top;
+});
+
+
+function add() {
+	document.getElementById("idDummy1").style.display = "none";
+	/*var test = document.getElementById('idMyImageName');
+	var cln = test.cloneNode(true);
+	document.body.appendChild(cln); 
+	*/
+	document.getElementById("idMyImageName2").style.display = "flex";
+}
+
+
+function remove() {
+	document.getElementById("idDummy1").style.display = "flex";
+	/*var test = document.getElementById('idMyImageName');
+	var cln = test.cloneNode(true);
+	document.body.appendChild(cln); 
+	*/
+	document.getElementById("idMyImageName2").style.display = "none";
+}
+
+}
---
+++ WebContent/script/share.js
@@ -0,0 +1,21 @@
+window.onload=()=>{ 
+
+var navElement0 = document.getElementById("idNav0");
+navElement0.innerHTML='<a href="file:///C:/Users/ndduc/Desktop/Eclipse_Git/myweb/WebContent/intro.html">Home</a>' ;
+
+var navElement1 = document.getElementById("idNav1");
+navElement1.innerHTML='<a href="file:///C:/Users/ndduc/Desktop/Eclipse_Git/myweb/WebContent/project.html">Project</a>' ;
+
+var navElement2 = document.getElementById("idNav2");
+navElement2.innerHTML='<a href="file:///C:/Users/ndduc/Desktop/Eclipse_Git/myweb/WebContent/intro.html">Hobby</a>' ;
+
+var navElement3 = document.getElementById("idNav3");
+navElement3.innerHTML='<a href="file:///C:/Users/ndduc/Desktop/Eclipse_Git/myweb/WebContent/intro.html">Equipment</a>' ;
+
+var navElement4 = document.getElementById("idNav4");
+navElement4.innerHTML='<a href="file:///C:/Users/ndduc/Desktop/Eclipse_Git/myweb/WebContent/intro.html">Info</a>' ;
+
+var navElement5 = document.getElementById("idNav5");
+navElement5.innerHTML='<a href="file:///C:/Users/ndduc/Desktop/Eclipse_Git/myweb/WebContent/mynote.html">Note</a>' ;
+
+};
